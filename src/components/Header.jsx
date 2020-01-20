55de201f68ceb685fb422a3c4fee67e96b02fb86
pane99 <pane99@live.com>
2020-01-20 18:31:32 -0500

    Create a React app: post-it

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import CollectionsBookmarkIcon from "@material-ui/icons/CollectionsBookmark";
+
+function Header() {
+  return (
+    <header>
+      <h1>
+        <CollectionsBookmarkIcon /> Post-it
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
